f65eea29ed0552c920ef460630ca8ac507572a42
sohanemon <m.sohanemon@gmail.com>
2023-05-30 10:15:54 +0600

    del: redundant icons

--- src/app/icons/index.js
+++
@@ -1,7 +0,0 @@
-export { default as ArticleIcon } from "./ArticleIcon";
-export { default as HomeIcon } from "./HomeIcon";
-export { default as chatIcon } from "./chatIcon";
-export { default as CreateIcon } from "./CreateIcon";
-export { default as CollapsIcon } from "./CollapsIcon";
-export { default as LogoIcon } from "./Logo";
-export { default as LogoutIcon } from "./LogoutIcon";

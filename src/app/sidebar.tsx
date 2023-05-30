48c9572f1b211e7153ee6e86e4d907c6acea0b93
sohanemon <m.sohanemon@gmail.com>
2023-05-30 03:39:22 +0000

    May 30, 2023, 9:39 AM

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Sidebar() {
   return (
     <section className='relative bg-themeSlate'>
-      <Image src={'/public/assets/bar.svg'} alt='' width={70} height={1024} />
+      <Image
+        className='absolute top-0 right-0'
+        src={'/assets/bar.svg'}
+        alt=''
+        width={70}
+        height={1024}
+      />
     </section>
   );
 }

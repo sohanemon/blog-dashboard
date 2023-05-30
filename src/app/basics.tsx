cf39f943fd36dd443102e62c73104b1b854908da
adritatori <adritatori456@gmail.com>
2023-05-30 23:24:54 +0600

    done basiscs

--- src/app/basics.tsx
+++ src/app/basics.tsx
@@ -11,7 +11,35 @@ export default function Basics() {
   return (
     <section className='relative bg-themeSlate flex-col items-center flex  w-full min-h-auto'>
   <div className='absolute z-50 flex flex-col md:flex-row'>
-   
+  <div className='w-[1000px] min-h-[1010px] rounded-lg bg-rgba-opacity m-5 mb-0'>
+          <div className='flex m-10 items-start'>
+          <ReactSVG src={"/assets/jesusIcon.svg"} />
+                <div className='ml-10 w-full'>
+                          <p className='text-xs font-medium leading-loose text-white  pb-0 mb-3'>Name of Prompt *</p>
+                          <input
+                  className="w-full p-3 rounded-md bg-transparent text-[10px] whitespace-normal align-top ml-1"
+                  placeholder="jesusgpt"
+                /> 
+                <p className='text-xs font-medium leading-loose text-white  pb-0 mb-3 mt-10'>Description * *</p>
+                          <input
+                  className="w-full h-[86px] p-3 rounded-md bg-transparent text-[10px] whitespace-normal align-top ml-1"
+                  placeholder="For Example: Generate personalized emails like a pro"
+                /> 
+                <p className='text-xs font-medium leading-loose text-white  pb-0 mb-3 mt-10'>Tags*</p>
+                          <input
+                  className="w-full p-3 rounded-md bg-transparent text-[10px] whitespace-normal align-top ml-1"
+                  placeholder="Search for Tags..."
+                /> 
+                <p className='text-xs font-medium leading-loose text-white  pb-0 mb-3 mt-10' >Bounty *</p>
+                          <input
+                  className="w-full p-3 rounded-md bg-transparent text-[10px] whitespace-normal align-top ml-1"
+                  placeholder="Individual Prompt"
+                /> 
+
+<button className='flex items-center bg-white rounded pl-8 pr-8 pt-2 pb-2 mt-10'><p className='black-text  text-[10px] '>Save</p></button>
+                </div>
+          </div>
+  </div>
    {/* preview and text ends*/}
   </div>
 <Image
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Basics from "./basics";
 import Prompt from "./prompt";
 
 export default function Home() {
-  return <main className='flex flex-col items-center grow'>
-    <Prompt/>
+  return <main className='flex flex-col  grow'>
+    {/* <Prompt/> */}
+    <Basics/>
   </main>;
 }

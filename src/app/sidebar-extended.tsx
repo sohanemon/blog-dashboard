1ba47cb90075046ab9806102d7832806bdb632a9
sohanemon <m.sohanemon@gmail.com>
2023-05-30 04:48:16 +0000

    May 30, 2023, 10:48 AM

---
+++ src/app/sidebar-extended.tsx
@@ -0,0 +1,9 @@
+export default function SidebarExtended() {
+  return (
+    <>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea fuga quae
+      fugit officia doloremque, error sunt veritatis est quidem. Itaque, natus
+      delectus. Expedita magnam hic quas vel ratione a commodi.
+    </>
+  );
+}

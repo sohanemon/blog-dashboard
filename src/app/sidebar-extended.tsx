cc0aad184dd5ae4da753acc0a2326c262405012b
sohanemon <m.sohanemon@gmail.com>
2023-05-30 05:05:32 +0000

    May 30, 2023, 11:05 AM

--- src/app/sidebar-extended.tsx
+++ src/app/sidebar-extended.tsx
@@ -3,15 +3,16 @@ import { ReactSVG } from 'react-svg';
 
 export default function SidebarExtended() {
   return (
-    <section className='border-r w-52 border-white/20'>
+    <section className='px-4 border-r w-52 py-7 border-white/20'>
       <div className='flex'>
-        <Image
-          className='p-3 bg-themeLightSlate'
-          src='/assets/camera-fill.svg'
-          alt=''
-          width={24}
-          height={24}
-        />
+        <div className='p-3 rounded-lg bg-themeLightSlate'>
+          <Image src='/assets/camera-fill.svg' alt='' width={24} height={24} />
+        </div>
+        <div>
+          <p className='text-xs font-semibold leading-tight text-white'>
+            Language Translation Assistant
+          </p>
+        </div>
       </div>
     </section>
   );
